6e0e2aec1ef901396b78d834be27cd41ed95120e
Yehor Sobko <yehor.sobko@nordstrom.com>
2019-04-04 17:05:14 +0300

    Added initial folders

---
+++ PhotosTest/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PhotosTest
+//
+//  Created by Egor Sobko on 4/4/19.
+//  Copyright © 2019 Egor Sobko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}

c42a9d5d39f920e5a9618aa8731c262e9d0d379d
Yehor Sobko <yehor.sobko@nordstrom.com>
2019-04-04 17:05:09 +0300

    Added initial folders

---
+++ Api/Api.h
@@ -0,0 +1,19 @@
+//
+//  Api.h
+//  Api
+//
+//  Created by Egor Sobko on 4/4/19.
+//  Copyright © 2019 Egor Sobko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Api.
+FOUNDATION_EXPORT double ApiVersionNumber;
+
+//! Project version string for Api.
+FOUNDATION_EXPORT const unsigned char ApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Api/PublicHeader.h>
+
+

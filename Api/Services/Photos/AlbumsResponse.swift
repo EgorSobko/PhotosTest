a7168ab284f603839409d2f5ddfdd08411224084
Yehor Sobko <yehor.sobko@nordstrom.com>
2019-04-05 00:15:45 +0300

    added fields to responses in photos service

--- Api/Services/Photos/AlbumsResponse.swift
+++ Api/Services/Photos/AlbumsResponse.swift
@@ -1,5 +1,7 @@
 import Foundation
 
 public struct AlbumsResponse: Decodable {
-    
+    let userId: Int
+    let id: Int
+    let title: Int
 }
--- Api/Services/Photos/PhotosResponse.swift
+++ Api/Services/Photos/PhotosResponse.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 public struct PhotosResponse: Decodable {
-    
+    let albumId: Int
+    let id: Int
+    let title: String
+    let url: URL?
+    let thumbnailUrl: URL?
 }

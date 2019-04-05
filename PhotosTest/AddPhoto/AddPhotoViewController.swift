d9d9ef1307b5b563c5490b6cef3e25b4472ce4bf
Yehor Sobko <yehor.sobko@nordstrom.com>
2019-04-05 14:06:11 +0300

    added empty files for adding new photo

---
+++ PhotosTest/AddPhoto/AddPhotoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AddPhotoViewController.swift
+//  PhotosTest
+//
+//  Created by Egor Sobko on 4/5/19.
+//  Copyright © 2019 Egor Sobko. All rights reserved.
+//
+
+import Foundation
---
+++ PhotosTest/AddPhoto/AddPhotoViewStateFactory.swift
@@ -0,0 +1,9 @@
+//
+//  AddPhotoViewStateFactory.swift
+//  PhotosTest
+//
+//  Created by Egor Sobko on 4/5/19.
+//  Copyright © 2019 Egor Sobko. All rights reserved.
+//
+
+import Foundation
